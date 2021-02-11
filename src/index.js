e415b95bec264908a577ebd8d668e51a2ec1deae
HooliganGS <gaga.borovikov.ilia12@gmail.com>
2021-02-11 09:09:37 +0300

    init:complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = String(n);
+    n.split('');
+    let newArray = [];
+    for (let i = 0; i < n.length; i++) {
+        newArray[n.length - i] = n[i];
+    }
+    newArray.splice(0, 1);
+    for (let i = 0; i < newArray.length; i++) {
+        if (newArray[i] === '-') {
+            newArray.pop();
+        }
+    }
+    return newArray.join('');
 }
